87260abf080197eed0bf13eceb45f3b2c86082ab
Lucas <luc4sgbsoar3s@gmail.com>
2025-11-27 08:25:23 -0300

    makes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,424 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+export default function Makeup() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <section className="tudo">
+      <header>
+        <div className="logo">
+          <img src="../SkinCare/logoSkincare.png" alt="logoSkincare.png" />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <a href="/"><p>Home</p></a>
+        <p>SkinCare</p>
+        <p>MakeUp</p>
+        <p>Dermatology</p>
+        <p>Products</p>
+
+        <div className="tracos">
+          <img src="../Logos/tracos.png" alt="tracos.png" />
         </div>
-      </main>
+      </header>
+
+      <div className="painel">
+        <img src="painel.png" alt="logoS.png" />
+      </div>
+
+<div className="comeco">
+  <div className="textow">
+<p> A maquiagem vai muito além de cores e produtos, ela é uma forma de expressão, autoconfiança e cuidado. Aqui, na Beleza que Brilha, acreditamos que cada pele tem sua própria história, e cada detalhe merece atenção especial.
+  <br />
+  <br />
+  Seja you iniciante ou apaixonada por makes, este é o seu espaço para descobrir como realçar sua beleza natural com as melhores dicas, tutoriais e produtos ideais para o seu tipo de pele.
+</p>
+</div>
+<div className="moleres">
+<img src="muleres.png" alt="" />
+</div>
+</div>
+
+<div className="frase">
+  <h1>'Domine sua make, conquiste seu dia.'</h1>
+</div>
+
+<div className="skins">
+  <h1>SKINS</h1>
+</div>
+
+<div className="pele">
+  <div className="img">
+    <img src="cara1.png" alt="" />
+  </div>
+  <div className="paleta1">
+  <img src="palheta1.png" alt="" />
+  </div>
+  <div className="textou">
+    <h1>PELE OLEOSA</h1>
+    <p>Prefira produtos oil-free e com textura leve.
+Use primer matificante antes da base para controlar a oleosidade.
+Aposte em bases com acabamento matte e pós translúcidos para selar.
+Evite produtos cremosos em excesso.
+</p>
+  </div>
+</div>
+
+
+<div className="pele2">
+  <div className="textou2">
+    <h1>PELE SECA</h1>
+    <p>Hidrate bem antes da make 
+A preparação da pele é essencial.
+Prefira bases hidratantes ou luminosas.
+Evite pós em excesso, que podem marcar linhas e deixar a pele com aspecto ressecado.
+Invista em iluminadores líquidos ou cremosos para um brilho natural.
+</p>
+  </div>
+  <div className="paleta2">
+  <img src="palheta2.png" alt="" />
+  </div>
+  <div className="img2">
+    <img src="cara2.png" alt="" />
+  </div>
+</div>
+
+
+<div className="pele">
+  <div className="img3">
+    <img src="ruiva.png" alt="" />
+  </div>
+  <div className="paleta1">
+  <img src="palheta3.png" alt="" />
+  </div>
+  <div className="textou">
+    <h1>PELE MISTA</h1>
+    <p>Use primer apenas na zona T para equilibrar a oleosidade.
+Aplique base semi-matte ou equilibrada
+ (nem muito seca, nem muito cremosa).
+Finalize com pó leve apenas nas áreas oleosas.
+</p>
+  </div>
+</div>
+
+
+<div className="pele2">
+  <div className="textou2">
+    <h1>PELE SENSÍVEL</h1>
+    <p>A pele sensível costuma reagir facilmente a produtos com fragrância, álcool ou corantes, apresentando vermelhidão e irritação.
+Prefira produtos hipoalergênicos e dermatologicamente testados.
+Escolha bases minerais ou BB creams suaves, sem perfume.
+Evite esponjas sujas e limpe bem os pincéis.
+</p>
+  </div>
+  <div className="paleta2">
+  <img src="palheta4.png" alt="" />
+  </div>
+  <div className="img4">
+    <img src="mulerC.png" alt="" />
+  </div>
+</div>
+
+
+<div className="pele">
+  <div className="img3">
+    <img src="veia.png" alt="" />
+  </div>
+  <div className="paleta1">
+  <img src="palheta5.png" alt="" />
+  </div>
+  <div className="textou">
+    <h1>PELE MADURA</h1>
+    <p>Invista em produtos hidratantes e iluminadores, que devolvem o viço natural.
+Use bases leves e fluídas, que não marquem rugas.
+Prefira corretivos cremosos e evite o excesso de pó.
+Dê preferência a tons suaves e naturais, que realçam sem pesar.
+</p>
+  </div>
+</div>
+
+<div className="pele2">
+  <div className="textou2">
+    <h1>PELE ACNEICA</h1>
+    <p>Use primer e base com controle de oleosidade, que ajudem a disfarçar imperfeições sem obstruir os poros.
+Prefira texturas leves e respiráveis, como bases líquidas ou em mousse.
+Evite produtos pesados e camadas grossas de maquiagem.
+</p>
+  </div>
+  <div className="paleta2">
+  <img src="palheta6.png" alt="" />
+  </div>
+  <div className="img4">
+    <img src="acne.png" alt="" />
+  </div>
+</div>
+
+<div className="skins">
+  <h1>PRODUTOS RECOMENDADOS</h1>
+</div>
+
+
+<div className="text">
+  <p> Se você gosta de maquiagem, sabe que alguns passos fazem toda a diferença no resultado final
+e preparar a pele é um deles! </p>
+</div>
+<div className="comeco">
+  <div className="textow">
+    <h1>PRIMERS</h1>
+    <p>Um primer serve para “preparar a pele” para a maquiagem, ajuda a suavizar textura, minimizar poros ou controlar brilho, fazer a maquiagem durar mais, ele geralmente é usado após os cuidados com a pele (limpeza, hidratação, protetor) e antes da base.</p>
+</div>
+<div className="primer">
+<img src="mulerDoPrime.png" alt="" />
+</div>
+</div>
+
+<div className="quebra"></div>
+
+  <div className="pele_oleosa">
+      <div className="textio">
+        <h1>PELE OLEOSA</h1>
+        <p>Prioridades: controle de brilho/oleosidade, minimizar poros, acabamento mais seco ou acetinado.
+          <br />
+          Dica de uso: escolha primers que tenham fórmulas “mattifying”, com textura leve, que absorvem ou controlam o sebo.</p>
+      </div>
+      <div className="imgs">
+        <img src="eudora_primer2.png" alt="" />
+        <img src="smashbox.png" alt="" />
+
+      </div>
+  </div>
+
+  <div className="quebra"></div>
+
+
+  <div className="pele_seca">
+      <div className="imgs">
+        <img src="glass_ruby.png" alt="" />
+        <img src="douborq.png" alt="" />
+      </div>
+      <div className="textio">
+        <h1>PELE SECA</h1>
+        <p>Prioridades: hidratação, evitar que a maquiagem marque peles secas ou escamas, acabamento mais suave. 
+          <br />
+          Dica de uso: use primer com agentes hidratantes, que “ancoram” a maquiagem na pele seca, e evite produtos muito matificantes que podem ressaltar “craquelados”.</p>
+      </div>
+  </div>
+
+  <div className="quebra"></div>
+
+
+  <div className="pele_mista">
+      <div className="textio">
+        <h1>PELE MISTA</h1>
+        <p>Prioridades: trata zonas oleosas e secas ao mesmo tempo (ex: zona T oleosa + bochechas mais secas).
+          <br />
+          Dica de uso: use “road-map” de duas zonas: primer matificante na zona T, primer hidratante ou normal nas outras áreas</p>
+      </div>
+      <div className="imgs">
+        <img src="master_prime.png" alt="" />
+        <img src="elf.png" alt="" />
+      </div>
+  </div>
+
+  <div className="quebra"></div>
+
+
+  <div className="pele_sensivel">
+      <div className="imgs">
+        <img src="pore_perfect4.png" alt="" />
+        <img src="cc_cream2.png" alt="" />
+      </div>
+      <div className="textio">
+        <h1>PELE SENSÍVEL</h1>
+        <p>Prioridades: evitar ingredientes que irritem (fragrância, álcool forte, óleos pesados), fórmulas “não-comedogênicas”, “sem perfume”, “dermatologicamente testadas”.
+          <br />
+          Dica de uso: verifique rotulagem “sensitive skin”, prefira primers com menos “extras” e mais simples. Faça teste num pequeno local antes de aplicar no rosto todo.</p>
+      </div>
+  </div>
+
+  <div className="quebra"></div>
+
+
+    <div className="pele_envelhecida">
+      <div className="textio">
+        <h1>PELE ENVELHECIDA</h1>
+        <p>Prioridades: suavizar linhas finas, textura desigual, zonas mais finas, talvez menos brilho forte.
+          <br />
+          Dica de uso: escolha primers com acabamento luminoso ou “soft-focus” (que dão aspecto mais “agradável”), com ingredientes que ajudam a suavizar textura. Evite fórmulas muito secas ou com silicone pesado que evidenciem linhas</p>
+      </div>
+      <div className="imgs">
+        <img src="una.png" alt="" />
+        <img src="db_primer.png" alt="" />
+      </div>
+  </div>
+
+  <div className="quebra"></div>
+
+
+  <div className="pele_acneica">
+      <div className="imgs">
+        <img src="acnover.png" alt="" />
+        <img src="dermotivin.png" alt="" />
+      </div>
+      <div className="textio">
+        <h1>PELE ACNEICA</h1>
+        <p>Prioridades: controlar oleosidade, minimizar poros e textura, evitar obstrução dos poros, usar fórmulas “oil-free”, “não-comedogênicas”.
+          <br />
+          Dica de uso: similar à pele oleosa + preferir fórmulas com ingredientes como ácido salicílico ou niacinamida se tolerar. Evitar fórmulas muito espessas ou ricas em óleos.</p>
+      </div>
+  </div>
+
+  <div className="skins">
+  <h1>INDICAÇÕES</h1>
+</div>
+
+<div className="titl">
+<h1>PELE OLEOSA</h1>
+<hr />
+</div>
+<div className="pele_oleosa">
+      <div className="textio">
+        <p>Priorize fórmulas matte, “oil-free”, que controlem brilho e poros, acabamento em pó ajuda a absorver oleosidade e selar a maquiagem.
+Aplicação: Usar um bom primer de controle de brilho + base ou pó, e selar a zona T com pó para maior durabilidade.</p>
+      </div>
+      <div className="imgs">
+        <img src="una.png" alt="" />
+        <img src="db_primer.png" alt="" />
+      </div>
+  </div>
+
+
+  <div className="titl">
+<hr />
+<h1>PELE SECA</h1>
+</div>
+<div className="pele_seca">
+      <div className="imgs">
+        <img src="clinique.png" alt="" />
+        <img src="nars.png" alt="" />
+      </div>
+      <div className="textio">
+        <p>Formulações que hidratam, acabamento mais luminoso ou natural e evitar produtos muito secos que evidenciem “craquelados”.
+        <br />
+        Aplicação: Bom hidratante + primer hidratante antes da base. Usar pouco pó ou apenas nas áreas que “brilham”.</p>
+      </div>
+  </div>
+
+
+  <div className="titl">
+<h1>PELE MISTA</h1>
+<hr />
+</div>
+<div className="pele_mista">
+      <div className="textio">
+        <p>Aqui há combinação: zonas oleosas + zonas mais secas. 
+          <br />
+          Ideal: produtos que equilibram ou usar diferentes produtos por zona.
+        <br />
+        Aplicação: Base líquida ou em pó leve nas bochechas (se mais secas), selar a zona T com pó – ou usar produto 2-em-1 conforme necessidade.</p>
+      </div>
+      <div className="imgs">
+        <img src="kiko.png" alt="" />
+        <img src="sephora.png" alt="" />
+      </div>
+  </div>
+
+
+  <div className="titl">
+<hr />
+<h1>PELE SENSÍVEL</h1>
+</div>
+<div className="pele_sensivel">
+      <div className="imgs">
+        <img src="elf_bb.png" alt="" />
+        <img src="kiko_milano.png" alt="" />
+      </div>
+      <div className="textio">
+        <p>Prioridades: evitar ingredientes que irritem (fragrância, álcool forte, óleos pesados), fórmulas “não-comedogênicas”, “sem perfume”, “dermatologicamente testadas”.
+          <br />
+          Dica de uso: verifique rotulagem “sensitive skin”, prefira primers com menos “extras” e mais simples. Faça teste num pequeno local antes de aplicar no rosto todo.</p>
+      </div>
+  </div>
+
+
+  <div className="titl">
+<h1>PELE ENVELHECIDA</h1>
+<hr />
+</div>
+<div className="pele_envelhecida">
+      <div className="textio">
+        <p>Busque acabamento “soft focus”, cobertura que suavize linhas finas sem acumular nelas. Evitar base excessivamente matte que evidencie textura.
+        <br />
+        Aplicação: Hidratação + primer “blur” ou iluminador leve, aplicar base líquida ou pó de boa qualidade, selar moderadamente com pó fino para manutenção.</p>
+      </div>
+      <div className="imgs">
+        <img src="una_natura_base.png" alt="" />
+        <img src="mac_powder.png" alt="" />
+      </div>
+  </div>
+
+
+  <div className="titl">
+<hr />
+<h1>PELE ACNEICA</h1>
+</div>
+<div className="pele_acneica">
+      <div className="imgs">
+        <img src="pix2.png" alt="" />
+        <img src="nars.png" alt="" />
+      </div>
+      <div className="textio">
+        <p>Prioridades: controlar oleosidade, minimizar poros e textura, evitar obstrução dos poros, usar fórmulas “oil-free”, “não-comedogênicas”.
+          <br />
+          Dica de uso: similar à pele oleosa + preferir fórmulas com ingredientes como ácido salicílico ou niacinamida se tolerar. Evitar fórmulas muito espessas ou ricas em óleos.</p>
+      </div>
+  </div>
+
+  <div className="quebra"></div>
+
+  <div className="final">
+    <p> Para uma maquiagem perfeita busque utilizar produtos que lhe agradam e agradam sua pele.
+<br />
+Use o pó com moderação para evitar acúmulo em linhas ou áreas secas.
+<br />
+Sempre verifique o resultado em diferentes luzes e mantenha os cuidados básicos de pele, limpeza, hidratação e proteção solar, para um acabamento perfeito.</p>
+
+  <img src="mulieres.png" alt="" />
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="Skincare">
+   <p>SKINCARE</p>
     </div>
+
+<div className="protetor_solar">
+    <hr />
+    <p>PROTETOR SOLAR, O MAIS MAIS</p>
+    <hr />
+</div>
+
+<div className="protetor">
+  <div className="textou">
+    <h1>SUNSCREEN</h1>
+    <p>O protetor solar é a defesa essencial e mais importante contra o envelhecimento precoce e o câncer de pele. Sua principal função é proteger contra os raios UVB (queimaduras) e UVA (responsáveis por rugas e manchas). O uso diário é fundamental para preservar a juventude da pele, prevenir o surgimento de manchas e manter a barreira cutânea saudável, devendo ser aplicado todos os dias, independentemente do clima.</p>
+    </div>
+    <div className="girl">
+    <img src="coreanGirl.png" alt="" />
+    </div>
+  
+</div>
+
+<div className="mais">
+  <h1>OS MAIS MELHORES</h1>
+</div>
+    
+    </section>
   );
 }
